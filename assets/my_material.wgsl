60440e5b5319df44599ea3df4b038cc6fd328853
Matthew <logicprojectsforfpgas@gmail.com>
2022-05-12 11:15:46 -0600

    Code Draft

--- assets/my_material.wgsl
+++ assets/my_material.wgsl
@@ -6,9 +6,24 @@ struct VertexOutput {
     [[location(2)]] uv: vec2<f32>;
 };
 
+struct MyMat {
+    alpha: f32;
+    color: vec4<f32>;
+};
+
+[[group(1), binding(0)]]
+var texture: texture_2d<f32>;
+[[group(1), binding(1)]]
+var our_sampler: sampler;
+
+[[group(1), binding(2)]]
+var<uniform> uniform_data: MyMat;
+
 [[stage(fragment)]]
 fn fragment(input: VertexOutput) -> [[location(0)]] vec4<f32> {
-    var output_color = vec4<f32>(input.uv,0.0,1.0);
+    var output_color = vec4<f32>(input.uv,0.0,uniform_data.alpha);
+    output_color = output_color * textureSample(texture, our_sampler, input.uv);
+    output_color = output_color * uniform_data.color;
     return output_color;
 }
 
--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,22 @@
 #![allow(clippy::type_complexity)]
 #![allow(clippy::too_many_arguments)]
 
+//use bevy::render::render_resource::std140::Std140;
+use bevy::render::render_resource::BufferSize;
+
+use bevy::render::render_resource::std140::Std140;
 use bevy::{
     ecs::system::{lifetimeless::SRes, SystemParamItem},
     prelude::*,
     reflect::TypeUuid,
     render::{
         camera::ScalingMode,
-        render_asset::{PrepareAssetError, RenderAsset},
+        render_asset::{PrepareAssetError, RenderAsset, RenderAssets},
         render_resource::{
-            BindGroup, BindGroupDescriptor, BindGroupLayout, BindGroupLayoutDescriptor,
+            std140::AsStd140, BindGroup, BindGroupDescriptor, BindGroupEntry, BindGroupLayout,
+            BindGroupLayoutDescriptor, BindGroupLayoutEntry, BindingResource, BindingType,
+            BufferBindingType, BufferInitDescriptor, BufferUsages,
+            SamplerBindingType, ShaderStages, TextureSampleType, TextureViewDimension,
         },
         renderer::RenderDevice,
     },
@@ -43,25 +50,49 @@ fn main() {
         })
         .add_plugin(WorldInspectorPlugin::new())
         .add_startup_system(spawn_camera)
+        .add_startup_system_to_stage(StartupStage::PreStartup, load_image)
         .add_system(toggle_inspector)
         .run();
+}
+
+#[derive(Deref)]
+pub struct Awesome(Handle<Image>);
+
+#[derive(TypeUuid, Clone)]
+#[uuid = "bc2f08eb-a0fb-43f1-a908-54871ea597d5"]
+struct MyMaterial {
+    alpha: f32,
+    color: Color,
+    image: Handle<Image>,
+}
+
+#[derive(Clone, AsStd140)]
+struct MyMaterialUniformData {
+    alpha: f32,
+    color: Vec4,
+}
+
+fn load_image(mut commands: Commands, assets: Res<AssetServer>) {
+    let awesome = assets.load("awesome.png");
+    commands.insert_resource(Awesome(awesome));
 }
 
 fn spawn_quad(
     mut commands: Commands,
     mut mesh_assets: ResMut<Assets<Mesh>>,
     mut my_material_assets: ResMut<Assets<MyMaterial>>,
+    awesome: Res<Awesome>,
 ) {
     commands.spawn_bundle(MaterialMesh2dBundle {
         mesh: mesh_assets.add(Mesh::from(shape::Quad::default())).into(),
-        material: my_material_assets.add(MyMaterial),
+        material: my_material_assets.add(MyMaterial {
+            alpha: 0.5,
+            color: Color::RED,
+            image: awesome.clone(),
+        }),
         ..default()
     });
 }
-
-#[derive(TypeUuid, Clone)]
-#[uuid = "bc2f08eb-a0fb-43f1-a908-54871ea597d5"]
-struct MyMaterial;
 
 struct MyMaterialGPU {
     bind_group: BindGroup,
@@ -75,7 +106,36 @@ impl Material2d for MyMaterial {
     fn bind_group_layout(render_device: &RenderDevice) -> BindGroupLayout {
         render_device.create_bind_group_layout(&BindGroupLayoutDescriptor {
             label: None,
-            entries: &[],
+            entries: &[
+                BindGroupLayoutEntry {
+                    binding: 0,
+                    visibility: ShaderStages::FRAGMENT,
+                    ty: BindingType::Texture {
+                        multisampled: false,
+                        sample_type: TextureSampleType::Float { filterable: true },
+                        view_dimension: TextureViewDimension::D2,
+                    },
+                    count: None,
+                },
+                BindGroupLayoutEntry {
+                    binding: 1,
+                    visibility: ShaderStages::FRAGMENT,
+                    ty: BindingType::Sampler(SamplerBindingType::Filtering),
+                    count: None,
+                },
+                BindGroupLayoutEntry {
+                    binding: 2,
+                    visibility: ShaderStages::FRAGMENT,
+                    ty: BindingType::Buffer {
+                        ty: BufferBindingType::Uniform,
+                        has_dynamic_offset: false,
+                        min_binding_size: BufferSize::new(
+                            MyMaterialUniformData::std140_size_static() as u64,
+                        ),
+                    },
+                    count: None,
+                },
+            ],
         })
     }
 
@@ -88,20 +148,57 @@ impl Material2d for MyMaterial {
 impl RenderAsset for MyMaterial {
     type ExtractedAsset = MyMaterial;
     type PreparedAsset = MyMaterialGPU;
-    type Param = (SRes<RenderDevice>, SRes<Material2dPipeline<MyMaterial>>);
+    type Param = (
+        SRes<RenderDevice>,
+        SRes<Material2dPipeline<MyMaterial>>,
+        SRes<RenderAssets<Image>>,
+    );
 
     fn extract_asset(&self) -> MyMaterial {
         self.clone()
     }
 
     fn prepare_asset(
-        _extracted_asset: MyMaterial,
-        (render_device, pipeline): &mut SystemParamItem<Self::Param>,
+        extracted_asset: MyMaterial,
+        (render_device, pipeline, images): &mut SystemParamItem<Self::Param>,
     ) -> Result<MyMaterialGPU, PrepareAssetError<MyMaterial>> {
+        let (view, sampler) = if let Some(result) = pipeline
+            .mesh2d_pipeline
+            .get_image_texture(images, &Some(extracted_asset.image.clone()))
+        {
+            result
+        } else {
+            return Err(PrepareAssetError::RetryNextUpdate(extracted_asset));
+        };
+
+        let uniform_data = MyMaterialUniformData {
+            alpha: extracted_asset.alpha,
+            color: extracted_asset.color.as_linear_rgba_f32().into(),
+        };
+
+        let buffer = render_device.create_buffer_with_data(&BufferInitDescriptor {
+            label: None,
+            contents: uniform_data.as_std140().as_bytes(),
+            usage: BufferUsages::UNIFORM | BufferUsages::COPY_DST,
+        });
+
         let bind_group = render_device.create_bind_group(&BindGroupDescriptor {
             label: None,
             layout: &pipeline.material2d_layout,
-            entries: &[],
+            entries: &[
+                BindGroupEntry {
+                    binding: 0,
+                    resource: BindingResource::TextureView(view),
+                },
+                BindGroupEntry {
+                    binding: 1,
+                    resource: BindingResource::Sampler(sampler),
+                },
+                BindGroupEntry {
+                    binding: 2,
+                    resource: buffer.as_entire_binding(),
+                },
+            ],
         });
         Ok(MyMaterialGPU { bind_group })
     }
